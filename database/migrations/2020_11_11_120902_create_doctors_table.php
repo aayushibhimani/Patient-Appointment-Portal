42c9f42548bb1c8d7ad2b960cedcd31ea412864d
Krutika Bhalla <krutika.bhalla@somaiya.edu>
2020-12-18 00:07:00 +0530

    doctor table nullables added :rocket:

--- database/migrations/2020_11_11_120902_create_doctors_table.php
+++ database/migrations/2020_11_11_120902_create_doctors_table.php
@@ -15,7 +15,6 @@ class CreateDoctorsTable extends Migration
     {
         Schema::create('doctors', function (Blueprint $table) {
             $table->id();
-<<<<<<< HEAD
             $table->date('dob')->nullable();
             $table->string('gender')->nullable();
             $table->string('profile_pic')->nullable();
@@ -23,36 +22,22 @@ class CreateDoctorsTable extends Migration
             $table->text('clinic_address')->nullable();
             $table->unsignedBigInteger('clinic_no')->nullable();
             $table->string('specialization')->nullable();
-            $table->string('education')->nullable();
-            $table->string('education_degree')->nullable();
-            $table->string('registration_name')->nullable();
-            $table->string('registration_year')->nullable();
-            $table->unsignedBigInteger('user_id')->nullable();
-=======
-            $table->date('dob');
-            $table->string('gender');
-            $table->string('profile_pic');
-            $table->string('clinic_name');
-            $table->text('clinic_address');
-            $table->unsignedBigInteger('clinic_no');
-            $table->string('specialization');
             // edu details
-            $table->string('education_college');
-            $table->string('year_of_completion');
-            $table->string('education_degree');
+            $table->string('education_college')->nullable();
+            $table->string('year_of_completion')->nullable();
+            $table->string('education_degree')->nullable();
             //end edu details
 
             //experience details
-            $table->string('hospital_name');
-            $table->string('start_date');
-            $table->string('end_date');
-            $table->string('destination');
+            $table->string('hospital_name')->nullable();
+            $table->string('start_date')->nullable();
+            $table->string('end_date')->nullable();
+            $table->string('destination')->nullable();
             // end exp details
 
-            $table->string('registration_name');
-            $table->string('registration_year');
+            $table->string('registration_name')->nullable();
+            $table->string('registration_year')->nullable();
             $table->unsignedBigInteger('user_id');
->>>>>>> 81e84f197a29d17a63f318af3e7b374ea61ad889
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
