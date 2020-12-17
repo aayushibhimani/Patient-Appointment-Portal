81e84f197a29d17a63f318af3e7b374ea61ad889
Krutika Bhalla <krutika.bhalla@somaiya.edu>
2020-12-17 23:57:02 +0530

    doctor profile

--- database/migrations/2020_11_11_120902_create_doctors_table.php
+++ database/migrations/2020_11_11_120902_create_doctors_table.php
@@ -22,8 +22,19 @@ class CreateDoctorsTable extends Migration
             $table->text('clinic_address');
             $table->unsignedBigInteger('clinic_no');
             $table->string('specialization');
-            $table->string('education');
+            // edu details
+            $table->string('education_college');
+            $table->string('year_of_completion');
             $table->string('education_degree');
+            //end edu details
+
+            //experience details
+            $table->string('hospital_name');
+            $table->string('start_date');
+            $table->string('end_date');
+            $table->string('destination');
+            // end exp details
+
             $table->string('registration_name');
             $table->string('registration_year');
             $table->unsignedBigInteger('user_id');
--- routes/web.php
+++ routes/web.php
@@ -26,11 +26,14 @@ Route::get('/change-password', [App\Http\Controllers\HomeController::class, 'cha
 
 
 //Route::get('/doctor-dashboard', [App\Http\Controllers\DoctorsController::class, 'index'])->name('doctor-dashboard');
+<<<<<<< Updated upstream
 Route::get('/appointments', [App\Http\Controllers\DoctorsController::class, 'appointments'])->name('doctor-appointments');
 Route::get('/my-patients', [App\Http\Controllers\DoctorsController::class, 'patients'])->name('doctor-patients');
 Route::get('/doctor-profile-settings', [App\Http\Controllers\DoctorsController::class, 'profileSettings'])->name('doctor-profile-settings');
 Route::get('/patient-profile', [App\Http\Controllers\DoctorsController::class, 'patientProfile'])->name('patient-profile');
 Route::get('/schedule-timings', [App\Http\Controllers\DoctorsController::class, 'scheduleTimings'])->name('schedule-timings');
+=======
+>>>>>>> Stashed changes
 
 
 
@@ -53,9 +56,16 @@ Route::get('/booking', [App\Http\Controllers\PatientsController::class, 'booking
 
 Route::middleware(['auth','doctor'])->group(function(){
    // Route::resource('doctor', 'App\Http\Controllers\DoctorsController');
+    Route::get('/appointments', [App\Http\Controllers\DoctorsController::class, 'appointments'])->name('doctor-appointments');
+    Route::get('/my-patients', [App\Http\Controllers\DoctorsController::class, 'patients'])->name('doctor-patients');
+    Route::get('/doctor-profile-settings', [App\Http\Controllers\DoctorsController::class, 'profileSettings'])->name('doctor-profile-settings');
+    Route::post('/doctor-profile-settings', [App\Http\Controllers\DoctorsController::class, 'store'])->name('store-settings');
+    Route::get('/patient-profile', [App\Http\Controllers\DoctorsController::class, 'patientProfile'])->name('patient-profile');
+
 });
 
 Route::middleware(['auth','patient'])->group(function(){
     // Route::resource('patient', 'App\Http\Controllers\PatientsController');
     Route::get('/patient-profile-settings', [App\Http\Controllers\PatientsController::class, 'profileSettings'])->name('patient-profile-settings');
+
 });
